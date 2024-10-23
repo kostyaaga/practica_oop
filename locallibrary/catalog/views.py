c3417effa38dd5d13c5956e598ea519eb60e320d
aga <kostyafan2006@gmail.com>
2024-10-23 17:58:16 +0700

    books+auhtors

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 def index(request):
     num_books=Book.objects.all().count()
@@ -13,3 +15,21 @@ def index(request):
                  'num_instances':num_instances,
                  'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
