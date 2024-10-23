8438860e206aad2c521e01d903e52fd31841305b
aga <kostyafan2006@gmail.com>
2024-10-23 14:47:32 +0700

    new

--- design/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, login, singin
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('login', login, name='login'),
-    path('singin', singin, name='singin'),
-]
